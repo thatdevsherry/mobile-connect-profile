26c0454a0442bef7044c1f646f17161b0a3c929e
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-01-11 09:56:35 +0500

    Add authentication failed error class

---
+++ src/main/java/com/ufone/api/errors/AuthenticationFailed.java
@@ -0,0 +1,34 @@
+package com.ufone.api.errors;
+
+import com.ufone.api.errors.BaseErrorResponse;
+import com.ufone.api.request.Request;
+
+import javax.ws.rs.core.Response;
+
+import java.io.UnsupportedEncodingException;
+
+public class AuthenticationFailed extends BaseErrorResponse {
+        private final String error = "access_denied";
+        private final String errorDescription = "User failed to authenticate";
+        private String baseResponse;
+
+        @Override
+        public String getErrorTitle() {
+                return this.error;
+        }
+
+        @Override
+        public String getErrorDescription() {
+                return this.errorDescription;
+        }
+
+        public Response buildAndReturnResponse(Request request)
+            throws UnsupportedEncodingException {
+                AuthenticationFailed errorResponse = new AuthenticationFailed();
+                baseResponse = errorResponse.buildBaseErrorResponse(request.getRedirectURI());
+                baseResponse = errorResponse.addStateQueryParam(baseResponse, request.getState());
+                baseResponse = errorResponse.addCorrelationIDQueryParam(
+                    baseResponse, request.getCorrelationID());
+                return errorResponse.returnResponse(baseResponse);
+        }
+}
