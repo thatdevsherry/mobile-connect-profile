6baff6dcb4883eecbec3f499f449e27900c3aff4
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-01-11 10:15:41 +0500

    Generate random code using apache commons text

--- src/main/java/com/ufone/api/authorization_code/AuthorizationCodeResponse.java
+++ src/main/java/com/ufone/api/authorization_code/AuthorizationCodeResponse.java
@@ -1,11 +1,16 @@
 package com.ufone.api.authorization_code;
 
 import javax.ws.rs.core.Response;
+import org.apache.commons.text.RandomStringGenerator;
 
 public class AuthorizationCodeResponse {
         private String responseString;
         public String generateCode() {
-                return "sOmeRanDomCoDe";
+                char[][] codeFormat = {{'0', '9'}, {'A', 'Z'}, {'a', 'z'}};
+                RandomStringGenerator generator =
+                    new RandomStringGenerator.Builder().withinRange(codeFormat).build();
+                String code = generator.generate(30);
+                return code;
         }
         public Response buildResponse(
             String redirectURI, String state, String nonce, String correlationID) {
