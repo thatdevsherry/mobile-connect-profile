327fa8c97772d926564e4890feedbf07ee8def9e
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-01-12 20:44:51 +0500

    Add single method that calls all validators

--- src/main/java/com/ufone/api/validation/ITokenRequestValidation.java
+++ src/main/java/com/ufone/api/validation/ITokenRequestValidation.java
@@ -19,10 +19,13 @@
  */
 package com.ufone.api.validation;
 
+import com.ufone.api.request.TokenEndpointRequest;
 import com.ufone.api.exceptions.InvalidContentTypeException;
 import com.ufone.api.exceptions.InvalidGrantTypeException;
 
 public interface ITokenRequestValidation {
+        public boolean isRequestValid(TokenEndpointRequest request)
+            throws InvalidGrantTypeException, InvalidContentTypeException;
         public boolean validateGrantType(String grantType) throws InvalidGrantTypeException;
         public boolean validateAuthorizationCode(String authorizationCode);
         public boolean validateRedirectURI(String redirectURI);
