e8002166bf5f2334092ded7d1bf9e06f5b81212b
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-01-12 20:45:44 +0500

    Add impl. of single method to call all validators

--- src/main/java/com/ufone/api/validation/TokenRequestValidation.java
+++ src/main/java/com/ufone/api/validation/TokenRequestValidation.java
@@ -19,10 +19,20 @@
  */
 package com.ufone.api.validation;
 
+import com.ufone.api.request.TokenEndpointRequest;
 import com.ufone.api.exceptions.InvalidContentTypeException;
 import com.ufone.api.exceptions.InvalidGrantTypeException;
 
 public class TokenRequestValidation implements ITokenRequestValidation {
+        public boolean isRequestValid(TokenEndpointRequest request)
+            throws InvalidGrantTypeException, InvalidContentTypeException {
+                validateGrantType(request.getGrantType());
+                validateAuthorizationCode(request.getAuthorizationCode());
+                validateRedirectURI(request.getRedirectURI());
+                validateContentType(request.getContentType());
+                return true;
+        }
+
         public boolean validateGrantType(String grantType) throws InvalidGrantTypeException {
                 if (grantType.equals("authorization_code")) {
                         return true;
@@ -30,10 +40,12 @@ public class TokenRequestValidation implements ITokenRequestValidation {
                         throw new InvalidGrantTypeException();
                 }
         }
+
         public boolean validateAuthorizationCode(String authorizationCode) {
                 // testing
                 return true;
         }
+
         // the redirect uri validation could be put into a util class so it wouldn't need to be
         // repeated
         public boolean validateRedirectURI(String redirectURI) {
