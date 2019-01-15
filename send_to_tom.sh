3e3cd396a4800f9f0a57e6af32d7d8f8ab2ea038
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-01-15 09:20:31 +0500

    Update tomcat script to send over new war files

--- send_to_tom.sh
+++ send_to_tom.sh
@@ -1,6 +1,8 @@
 # Maven Build
 mvn clean test verify
 # Copy to tommcat
-sudo cp target/mobile-connect-server.war /usr/share/tomcat8/webapps/oidc.war
+sudo cp authorization-endpoint/target/authorization-endpoint.war /usr/share/tomcat8/webapps/
+sudo cp token-endpoint/target/token-endpoint.war /usr/share/tomcat8/webapps/
+sudo cp discovery-endpoint/target/discovery-endpoint.war /usr/share/tomcat8/webapps/
 # Restart tomcat
 sudo systemctl restart tomcat8
