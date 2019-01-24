07b32375e23032e05518043a1a74d64840eacbd7
Shehriyar Qureshi <SShehriyar266@gmail.com>
2019-01-24 14:58:18 +0500

    Add methods to query db and get authenticator
    Add methods in Policy Engine which query a db to see what
    authenticator to use for the request.

--- authorization-endpoint/src/main/java/com/ufone/api/policy_engine/AbstractPolicyEngine.java
+++ authorization-endpoint/src/main/java/com/ufone/api/policy_engine/AbstractPolicyEngine.java
@@ -19,15 +19,87 @@
  */
 package com.ufone.api.policy_engine;
 
-/* You are to implement this
- * interface to configure your own Policy Engine.
- *
- * I think the best way in which this would be flexible and to follow the principle of pluggable
- * authenticators is to implement a database call. That way we can easily assign mappings to
- * individual service providers. The Policy Engine would then query the database and the response
- * returned would be the type of authenticator to use.
- */
+import java.sql.*;
+
+import java.sql.SQLException;
+import java.lang.ClassNotFoundException;
+
+import java.util.Properties;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public abstract class AbstractPolicyEngine {
-        public abstract String AuthenticatorSelection(
-            String cliendID, String redirectURI, String levelOfAssurance);
+        /*
+         * Query the Policy Engine database to figure out what authenticator to use.
+         *
+         * @param clientID value of client_id query parameter.
+         *
+         * @param redirectURI value of redirect_uri query parameter.
+         *
+         * @param levelOfAssurance value of acr_values query parameter.
+         *
+         * @return authenticator String which is the name of the authenticator to use.
+         */
+        public String AuthenticatorSelection(
+            String clientID, String redirectURI, String levelOfAssurance) {
+                String authenticatorToUse = null;
+                Properties properties = new Properties();
+
+                // Load values from config.properties, throw exception if something goes wrong
+                try {
+                        properties.load(this.getClass().getClassLoader().getResourceAsStream(
+                            "/config.properties"));
+                } catch (Exception e) {
+                        // raise appropriate exception and catch it in the handler to call the
+                        // correct response class
+                }
+
+                try {
+                        authenticatorToUse = getAuthenticatorToUse(
+                            clientID, redirectURI, levelOfAssurance, properties);
+                } catch (Exception e) {
+                }
+
+                return authenticatorToUse;
+        }
+
+        public String getAuthenticatorToUse(
+            String clientID, String redirectURI, String levelOfAssurance, Properties properties) {
+                String authenticatorToUse = null;
+                Connection connection = null;
+
+                try {
+                        // this shouldn't be required on newer versions but this project doesn't
+                        // seem to work without this for me
+                        Class.forName(properties.getProperty("databaseDriver"));
+
+                        connection = DriverManager.getConnection(
+                            properties.getProperty("PolicyEngineDatabase"),
+                            properties.getProperty("databaseUser"),
+                            properties.getProperty("databaseUserPassword"));
+                        PreparedStatement statement = connection.prepareStatement(
+                            properties.getProperty("getAuthenticatorToUse"));
+                        statement.setString(1, clientID);
+                        statement.setString(2, redirectURI);
+                        statement.setString(3, levelOfAssurance);
+                        ResultSet resultSet = statement.executeQuery();
+                        while (resultSet.next()) {
+                                authenticatorToUse = resultSet.getString(1);
+                        }
+                        resultSet.close();
+                        statement.close();
+                        return authenticatorToUse;
+                } catch (SQLException e) {
+                        // TODO: Appropriate Response pls
+                        return null;
+                } catch (ClassNotFoundException e) {
+                        // TODO: Appropriate Response pls
+                        return null;
+                }
+        }
 }
